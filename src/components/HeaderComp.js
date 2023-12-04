8b293c27f04d0422e98f71d974aea4aa1a514a88
shulgup <shulbhi1810@gmail.com>
2023-12-04 12:42:59 +0530

    new updated_name

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -43,7 +43,7 @@ export const HeaderComp = () => {
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Cinema Mate
+            Cinemate
           </span>
         </Link>
 
